b00fe7be53da582e28033074321d3200cc745a6d
Michal Grno <m93a.cz@gmail.com>
2021-05-11 20:26:42 +0200

    šedivé ikonky v ldev mapě

--- src/utils/icon.tsx
+++ src/utils/icon.tsx
@@ -2,4 +2,4 @@ import * as React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
-export const icon = (i: IconDefinition) => <><FontAwesomeIcon icon={i} /> &nbsp;</>;
+export const icon = (i: IconDefinition) => <><FontAwesomeIcon icon={i} className="icon" /> &nbsp;</>;
