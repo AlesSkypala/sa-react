78a49c9e5f2fa22e1b4325dd2d7d2bd4fc5e035e
Richard Ivanek <richard.ivanek@gmail.com>
2021-05-22 15:22:22 +0200

    Tab color fix

--- src/components/HiddenGraphs.tsx
+++ src/components/HiddenGraphs.tsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Dropdown } from 'react-bootstrap';
 import * as React from 'react';
 import { connect, unhide_graphs, ReduxProps, remove_graphs } from '../redux';
-import './HiddenGraphs.css';
+import './HiddenGraphs.scss';
 
 
 const dispatchProps = {
